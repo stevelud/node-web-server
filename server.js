6d4008dec4c47f6f104e5996bed327c1fd5a4bc5
Steven Ludvickson <ludvickson@gmail.com>
2021-12-30 11:00:03 -0800

    added api page, login partial

--- server.js
+++ server.js
@@ -32,21 +32,10 @@ hbs.registerHelper('getCurrentYear', () => {
 
 app.get('/', (req, res) => {
   res.status(200);
-  // res.type('text/plain')
   res.render('home.hbs', {
     pageTitle:'Home Page',
     welcomeMessage:'Welcome to my Website',
   });
-  // console.log(req.headers)
-  // console.log(req.accepts)
-  // console.log(req.ip)
-  // console.log(req.path)
-  // console.log(req.hostname)
-  // console.log(req.xhr)
-  // console.log(req.protocol)
-  // console.log(req.secure)
-  // console.log(req.url)
-  // console.log(req.body)
 });
 
 app.post('/process-login', (req, res) => {
@@ -59,21 +48,15 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/form', (req, res) => {
-  res.render('form.hbs', {
-    pageTitle:'Test Form',
-  });
-});
-
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
     pageTitle:'Projects',
   });
 });
 
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage:'unable to handle request'
+app.get('/api', (req, res) => {
+  res.render('api.hbs', {
+    pageTitle:'API',
   });
 });
 
